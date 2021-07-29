6246a396430c7561d8d535f39ffe560b6af4d2d7
Miika Härmä <harmamiika@gmail.com>
2021-07-29 23:45:52 +0300

    added header

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Container from '@material-ui/core/Container'
 
+import Header from './Header'
 import AddNote from './AddNote'
 import NoteList from './NoteList'
 
 const App = () => {
     return (
         <div>
+            <Header />
             <Container style={{ backgroundColor: "white" }}>
                 <AddNote />
                 <NoteList />
