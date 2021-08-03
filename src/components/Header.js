de704bc47fc515102e427d0764970a202609b25b
Miika Härmä <harmamiika@gmail.com>
2021-08-03 23:09:09 +0300

    added app architechture for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -30,10 +29,18 @@ const Header = () => {
                 <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                     <MenuIcon />
                 </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                    News
+                <Typography variant="h5" className={classes.title}>
+                    Notes App by Miika
                 </Typography>
-                <Button color="inherit">Login</Button>
+                <Typography variant="h6" className={classes.title}>
+                    Create Notes
+                </Typography>
+                <Typography variant="h6" className={classes.title}>
+                    NoteArchive - completed notes
+                </Typography>
+                <Typography variant="h6" className={classes.title}>
+                    Info
+                </Typography>
             </Toolbar>
         </AppBar>
     )
