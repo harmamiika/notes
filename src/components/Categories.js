d903de1645a4f457966337e8ec1c789e555194b0
Miika Härmä <harmamiika@gmail.com>
2021-08-07 15:31:47 +0300

    added category chips to select a diplay category

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,39 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
+import Chip from '@material-ui/core/Chip';
+
 const Categories = () => {
+    const [viewCategory, setViewCategory] = useState(null)
+
     const notes = useSelector(state => state.notes)
 
-    console.log(notes, 'notes123123')
-
-    const getCategories = () => {
-        if (!notes || notes === []) {
-            return null
+    const categories = notes.reduce((categoryArray, note) => {
+        if (categoryArray.includes(note.category)) {
+            return categoryArray
         }
+        return categoryArray.concat(note.category)
+    }, [])
 
-        return notes.reduce((categoryArray, note) => {
-            if (categoryArray.includes(note.category)) {
-                return categoryArray
-            }
-            return categoryArray.concat(note.category)
-        }, [])
-    }
 
-    const categories = getCategories()
+    console.log(notes, 'notes')
+
+
     console.log(categories, 'categories1231323')
 
     const renderCategories = () => {
         return categories.map(category => {
+            if (category === undefined || null) {
+                return null
+            }
             return (
-                <div>{category}</div>
+                <Chip
+                    key={category}
+                    label={category}
+                    clickable
+                    color="primary"
+                    onClick={() => setViewCategory(category)}
+                />
             )
         })
     }
+
 
     return (
         <div>
             <h1>Categories</h1>
             {renderCategories()}
+            {viewCategory ? <h2>{viewCategory}</h2> : null}
+
+
         </div>
     )
 }
