71cb33e45a21cf92e78fa874bf2bb97fd5c149ec
Miika Härmä <harmamiika@gmail.com>
2021-08-16 18:50:52 +0300

    made radio buttons and date picker display real nicely

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -10,6 +10,7 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
+import './AddNote.css'
 import { addNote } from '../actions/index'
 
 const AddNote = () => {
@@ -57,34 +58,40 @@ const AddNote = () => {
                 variant="outlined"
                 autoFocus
             />
-            <FormControl component="fieldset">
-                <FormLabel component="legend">Select category</FormLabel>
-                <RadioGroup aria-label="gender" name="gender1" value={category} onChange={handleChange}>
-                    <FormControlLabel value="Todos" control={<Radio />} label="Todos" />
-                    <FormControlLabel value="Books" control={<Radio />} label="Books" />
-                    <FormControlLabel value="Movies" control={<Radio />} label="Movies" />
-                    <FormControlLabel value="Shows" control={<Radio />} label="Shows" />
-                    <FormControlLabel value="Events" control={<Radio />} label="Events" />
-                    <FormControlLabel value="Goals" control={<Radio />} label="Goals" />
-                </RadioGroup>
-            </FormControl>
-            <TextField
-                onKeyPress={sendOnEnterPress}
-                onChange={(e) => setDeadline(e.target.value)}
-                value={deadline}
-                id="date"
-                label="Deadline (optional)"
-                type="date"
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
+            <div className='form-row'>
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Select category</FormLabel>
+                    <RadioGroup aria-label="gender" name="gender1" value={category} onChange={handleChange}>
+                        <div>
+                            <FormControlLabel value="Todos" control={<Radio />} label="Todos" />
+                            <FormControlLabel value="Books" control={<Radio />} label="Books" />
+                            <FormControlLabel value="Movies" control={<Radio />} label="Movies" />
+                            <FormControlLabel value="Shows" control={<Radio />} label="Shows" />
+                            <FormControlLabel value="Events" control={<Radio />} label="Events" />
+                            <FormControlLabel value="Goals" control={<Radio />} label="Goals" />
+                        </div>
+                    </RadioGroup>
+                </FormControl>
+                <TextField
+                    className='deadline-field'
+                    onKeyPress={sendOnEnterPress}
+                    onChange={(e) => setDeadline(e.target.value)}
+                    value={deadline}
+                    id="date"
+                    label="Deadline (optional)"
+                    type="date"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </div>
             <Button type="submit" variant="contained"
                 color="primary" fullWidth>Add new Todo!</Button>
         </form>
     )
 }
 
+// deadline default jotain -> event handlerin aktivoitumisen jälkeen deadlinen arvo
 
 // deadline textfieldin default value datesta
 
