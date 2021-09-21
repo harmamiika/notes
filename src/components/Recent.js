89b62c2d5b0aca07c08d7fc021fb495b9f7bf7d1
Miika Härmä <harmamiika@gmail.com>
2021-09-21 13:00:41 +0300

    made recent notes to show dynamically based on last added note

--- src/components/Recent.js
+++ src/components/Recent.js
@@ -8,23 +8,32 @@ import NoteList from './NoteList';
 import { makeUsDate } from '../services/localStorageDB';
 
 const Recent = () => {
-  const todos = useSelector((state) =>
-    state.notes.filter((note) => note.category === 'Todos')
-  );
+  const notes = useSelector((state) => state.notes);
 
-  const recentTodos = todos.filter(
+  const recentNotes = notes.filter(
     (todo) =>
       (new Date().getTime() - makeUsDate(todo.createDate).getTime()) /
         (1000 * 60 * 60 * 24) <
       7
   );
 
+  console.log(recentNotes, 'recentNotes');
+
+  const category =
+    recentNotes.length > 0
+      ? recentNotes[recentNotes.length - 1].category
+      : null;
+
+  const notesByCategory = notes.filter(
+    (note) => note.category === notes[0].category
+  );
+
   return (
     <div>
       <Typography className="headline" variant="h3" component="h3">
-        Recent todos
+        Recent {category && category}
       </Typography>
-      <NoteList notes={recentTodos} completed={false} />
+      {category && <NoteList notes={notesByCategory} completed={false} />}
     </div>
   );
 };
